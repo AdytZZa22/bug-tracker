32f2c3e00f798a38c540c7d08d0b54710ca006e4
Adrian Terinte <adrian@superwpheroes.io>
2023-11-16 20:19:15 +0200

    Delete invitation after it was accepted

--- src/app/project/invite/route.ts
+++ src/app/project/invite/route.ts
@@ -5,7 +5,10 @@ import {getProjectBySlug, inviteUser} from "@/modules/project/project.service";
 import getUserByEmail from "@/modules/user/user.service";
 import {getServerSession} from "next-auth";
 import {authOptions} from "@/lib/auth";
+import prisma from "@/lib/prisma";
 
+
+export const dynamic = 'force-dynamic'
 export async function GET(request: NextRequest) {
 
     try {
@@ -14,20 +17,31 @@ export async function GET(request: NextRequest) {
             project_slug: string
         } = await validateSignedUrl(request.url)
 
-        console.log(data)
 
 
         const session = await getServerSession(authOptions)
 
         if(!session?.user) redirect('/login');
 
+
+
         const project = await getProjectBySlug(data.project_slug)
         const user = await getUserByEmail(data.email)
         if(!project || !user) redirect("/")
 
+
+        await prisma.projectInvitation.deleteMany({
+            where: {
+                project_id: project.id,
+                email: data.email
+            }
+        })
+
         const invitation = await inviteUser(user, project)
 
         if(!invitation) redirect("/")
+
+
 
 
         return NextResponse.redirect(new URL(`/project/${project.slug}`, request.url))
